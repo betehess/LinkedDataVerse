b56e0e1a8db5be837c4834e043f98bcc4b337ae5
Alexandre Bertails <alexandre@bertails.org>
2015-03-04 17:55:41 -0500

    with jsonld-js example

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -11,26 +11,36 @@ object ScalaJSExample extends js.JSApp {
 
   val n3Parser = new n3js.io.N3jsTurtleParser[Plantain]
 
+  val jsonLdParser = new jsonldjs.io.JsonLdJsParser[Plantain]
+
   def main(): Unit = {
     val paragraph = dom.document.createElement("p")
     paragraph.innerHTML = "<strong>It works!</strong>"
     dom.document.getElementById("playground").appendChild(paragraph)
 
-    val sr = new java.io.StringReader("""
+    val turtle = new java.io.StringReader("""
 @prefix c: <http://example.org/cartoons#>.
 c:Tom a c:Cat. 
 c:Jerry a c:Mouse;
         c:smarterThan c:Tom.
 """)
 
-    n3Parser.read(sr, "http://example.com").foreach { graph =>
+    n3Parser.read(turtle, "http://example.com").foreach { graph =>
       graph.triples.foreach(println)
     }
 
+    val jsonLd = new java.io.StringReader("""{
+  "http://schema.org/name": "Manu Sporny",
+  "http://schema.org/url": {"@id": "http://manu.sporny.org/"},
+  "http://schema.org/image": {"@id": "http://manu.sporny.org/images/manu.png"}
+}""")
+
+    jsonLdParser.read(jsonLd, "http://example.com").foreach { graph =>
+      graph.triples.foreach(println)
+    }
+
+
   }
 
-  /** Computes the square of an integer.
-   *  This demonstrates unit testing.
-   */
-  def square(x: Int): Int = x*x
+
 }
